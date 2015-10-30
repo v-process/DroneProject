fe02b844ed21a01625a8b1800623de83e94c6b77
v-process <ksc3168@naver.com>
2015-10-30 22:55:26 +0900

    PID 연동

--- PIDTest/app/src/main/java/com/example/kimseungchul/pidtest/PIDTest.java
+++ PIDTest/app/src/main/java/com/example/kimseungchul/pidtest/PIDTest.java
@@ -33,6 +33,11 @@ public class PIDTest extends Activity implements View.OnClickListener{
     Button down_btn2;
     Button up_btn3;
     Button down_btn3;
+    Button up_btn4;
+    Button down_btn4;
+
+    Button start_btn;
+    Button stop_btn;
     String kp_data;
     String ki_data;
 
@@ -45,7 +50,7 @@ public class PIDTest extends Activity implements View.OnClickListener{
     TextView output;
     String data;
     String[] arraydata;
-    int[] ctrldata = new int[8];
+    float[] ctrldata = new float[8];
 
 
     @Override
@@ -77,6 +82,10 @@ public class PIDTest extends Activity implements View.OnClickListener{
         down_btn2 = (Button) findViewById(R.id.down_btn2);
         up_btn3 = (Button) findViewById(R.id.up_btn3);
         down_btn3 = (Button) findViewById(R.id.down_btn3);
+        up_btn4 = (Button) findViewById(R.id.up_btn4);
+        down_btn4 = (Button) findViewById(R.id.down_btn4);
+        start_btn = (Button) findViewById(R.id.start_btn);
+        stop_btn = (Button) findViewById(R.id.stop_btn);
 
         up_btn.setOnClickListener(this);
         down_btn.setOnClickListener(this);
@@ -84,28 +93,54 @@ public class PIDTest extends Activity implements View.OnClickListener{
         down_btn2.setOnClickListener(this);
         up_btn3.setOnClickListener(this);
         down_btn3.setOnClickListener(this);
+        up_btn4.setOnClickListener(this);
+        down_btn4.setOnClickListener(this);
+        start_btn.setOnClickListener(this);
+        stop_btn.setOnClickListener(this);
+
 
 
 
         Thread worker = new Thread() {
             public void run() {
                 try {
-                    socket = new Socket("192.168.1.7", 5555);
-                    //socket = new Socket("172.16.101.111", 5555);
+                    //socket = new Socket("192.168.1.7", 5555);//집.
+                    //socket = new Socket("172.16.101.118", 5555);//해찬.
+                    socket = new Socket("192.168.4.1", 3002);//바나나드론.
                     socket_out = new PrintWriter(socket.getOutputStream(), true);
                     socket_in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
                 try {
+                    String message;
                     while (true) {
 
-                        kp_data = socket_in.readLine();
+                        char[] in = new char[50];
+                        socket_in.read(in, 0, in.length);
+                        final String input = new String(in,0,in.length);
+                        Log.i("data12345", " " + input);
+//                        arraydata = input.split(",");
+//                        dumpArray(arraydata);
 
+
+//                        while ((message = socket_in.readLine()) != null) {
+//                            output = (TextView) findViewById(R.id.yaw_text);
+//                            output.setText(message);
+//                            Log.i("data12345", " " + message);
+//
+//                        }
+//                        kp_data = socket_in.readLine();
+//
+//                        yaw_text.setText(kp_data);
+//
+//                        Log.i("data12345", " "+ kp_data);
+//
+//
                         kp_text.post(new Runnable() {
                             public void run() {
-                                //kp_text.setText(String.valueOf(kp_data));
-                                arraydata = kp_data.split("s");
+                                //yaw_text.setText(String.valueOf(kp_data));
+                                arraydata = input.split(",");
                                 dumpArray(arraydata);
                             }
                         });
@@ -128,7 +163,7 @@ public class PIDTest extends Activity implements View.OnClickListener{
         }
     }
 
-    public  void dumpArray(String[] array) {
+    public void dumpArray(String[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.format("array[%d] = %s%n", i, array[i]);
             Log.i("data123", " " + array[i]);
@@ -136,66 +171,62 @@ public class PIDTest extends Activity implements View.OnClickListener{
         settingData(array);
     }
 
-    public  void settingData(String[] array){
+    public void settingData(String[] array){
         for(int i = 0; i<array.length; i++){
-
             //적용
+            Log.i("test", " "+ array[i]);
             setdata[i].setText(array[i]);
+            Log.i("test2", " " + array[i]);
 
-            ctrldata[i] = Integer.valueOf(array[i]);
+            //ctrldata[i] = Float.valueOf(array[i]);
         }
-
     }
 
     @Override
     public void onClick(View v) {
             if (v == up_btn) {
-                int kp_int = ++ctrldata[3];
-                String data = String.valueOf(kp_int);
-                setdata[3].setText(data);
+                String data = "q";
                 socket_out.println(data);
             }
             else if (v == down_btn) {
-                int kp_int = --ctrldata[3];
-                String data = String.valueOf(kp_int);
-                setdata[3].setText(data);
+                String data = "a";
                 socket_out.println(data);
-            } else if (v == up_btn2) {
-                if (ki_data != null) {
-
-                    int ki_int = Integer.valueOf(ki_data);
-                    ++ki_int;
-                    ki_data = String.valueOf(ki_int);
-                    ki_text.setText(ki_data);
-                    socket_out.println(ki_data);
-                }
-            } else if (v == down_btn2) {
-                if (ki_data != null) {
-                    int ki_int = Integer.valueOf(ki_data);
-                    --ki_int;
-                    ki_data = String.valueOf(ki_int);
-                    ki_text.setText(ki_data);
-                    socket_out.println(ki_data);
-                }
-            } else if (v == up_btn3) {
-                if (ki_data != null) {
-
-                    int ki_int = Integer.valueOf(ki_data);
-                    ++ki_int;
-                    ki_data = String.valueOf(ki_int);
-                    ki_text.setText(ki_data);
-                    socket_out.println(ki_data);
-                }
-            } else if (v == down_btn3) {
-                if (ki_data != null) {
-                    int ki_int = Integer.valueOf(ki_data);
-                    --ki_int;
-                    ki_data = String.valueOf(ki_int);
-                    ki_text.setText(ki_data);
-                    socket_out.println(ki_data);
-                }
             }
-        }
+            else if (v == up_btn2) {
+                String data = "w";
+                socket_out.println(data);
+            }
+            else if (v == down_btn2) {
+                String data = "s";
+                socket_out.println(data);
+            }
+            else if (v == up_btn3) {
+                String data = "e";
+                socket_out.println(data);
+            }
+            else if (v == down_btn3) {
+                String data = "d";
+                socket_out.println(data);
+            }
+            else if (v == start_btn) {
+                String data = "b";
+                socket_out.println(data);
+            }
+            else if (v == stop_btn) {
+                String data = "x";
+                socket_out.println(data);
+            }
+            else if (v == up_btn4) {
+                String data = "t";
+                socket_out.println(data);
+            }
+
+            else if (v == down_btn4) {
+                String data = "g";
+                socket_out.println(data);
+            }
+
+    }
 
 
 }
